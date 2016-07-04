f810629e16056d91026989bd74d6ce52da8dca8e
Michael Pope <mpope.cr@gmail.com>
2016-07-04 11:50:01 -0700

    Add yamerl_the_fork package

--- mix.exs
+++ mix.exs
@@ -3,30 +3,18 @@ defmodule MarkYamill.Mixfile do
 
   def project do
     [app: :mark_yamill,
-     version: "0.1.0",
-     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     elixir: "~> 1.3",
+     version: "0.1.0",
+     start_permanent: Mix.env == :prod]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :yamerl_the_fork]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:yamerl_the_fork, "~> 0.3.3"}]
   end
 end
