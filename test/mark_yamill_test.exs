5316052186e7e7e30c06c739bfaf4823746f61fe
Michael Pope <mpope.cr@gmail.com>
2016-07-06 15:24:28 -0700

    Simplify names of tests and test data files

--- test/mark_yamill_test.exs
+++ test/mark_yamill_test.exs
@@ -7,9 +7,94 @@ defmodule MarkYamillTest do
     MarkYamill.decode(yaml)
   end
 
-  describe "parsing" do
-    test "compact_nested_mapping" do
-      actual = decode("compact_nested_mapping")
+  describe "From http://www.yaml.org/spec/1.2/spec.html, parsing Example" do
+    test "2.1.  Sequence of Scalars (ball players)" do
+      actual = decode("2.1")
+      expected = ["Mark McGwire", "Sammy Sosa", "Ken Griffey"]
+      assert actual == expected
+    end
+
+    test "2.2.  Mapping Scalars to Scalars (player statistics)" do
+      actual = decode("2.2")
+      expected = %{"hr" => 65, "avg" => 0.278, "rbi" => 147}
+      assert actual == expected
+    end
+
+    test "2.3.  Mapping Scalars to Sequences (ball clubs in each league)" do
+      actual = decode("2.3")
+      expected =
+        %{"american"=> ["Boston Red Sox", "Detroit Tigers", "New York Yankees"],
+          "national"=> ["New York Mets", "Chicago Cubs", "Atlanta Braves"]}
+      assert actual == expected
+    end
+
+    test "2.4.  Sequence of Mappings (players' statistics)" do
+      actual = decode("2.4")
+      expected =
+        [%{"name"=> "Mark McGwire", "hr"=> 65, "avg"=> 0.278},
+         %{"name"=> "Sammy Sosa", "hr"=> 63, "avg"=> 0.288}]
+      assert actual == expected
+    end
+
+    test "2.5. Sequence of Sequences" do
+      actual = decode("2.5")
+      expected =
+        [["name", "hr", "avg"],
+         ["Mark McGwire", 65, 0.278],
+         ["Sammy Sosa", 63, 0.288]]
+      assert actual == expected
+    end
+
+    test "2.6. Mapping of Mappings" do
+      actual = decode("2.6")
+      expected =
+        %{"Mark McGwire"=> %{"hr"=>65, "avg"=>0.278},
+          "Sammy Sosa"=> %{"hr"=>63, "avg"=>0.288}}
+      assert actual == expected
+    end
+
+    test "2.7.  Two Documents in a Stream (each with a leading comment)" do
+      actual = decode("2.7")
+      expected =
+        [["Mark McGwire", "Sammy Sosa", "Ken Griffey"],
+         ["Chicago Cubs", "St Louis Cardinals"]]
+      assert actual == expected
+    end
+
+    test "2.8.  Play by Play Feed from a Game" do
+      actual = decode("2.8")
+      expected =
+        [%{"time"=> "20:03:20", "player"=> "Sammy Sosa", "action"=> "strike (miss)"},
+         %{"time"=> "20:03:47", "player"=> "Sammy Sosa", "action"=> "grand slam"}]
+      assert actual == expected
+    end
+
+    test "2.9.  Single Document with Two Comments" do
+      actual = decode("2.9")
+      expected =
+        %{"hr"=>["Mark McGwire", "Sammy Sosa"],
+          "rbi"=>["Sammy Sosa", "Ken Griffey"]}
+      assert actual == expected
+    end
+
+    test "2.10.  Node for 'Sammy Sosa' appears twice in this document" do
+      actual = decode("2.10")
+      expected =
+        %{"hr"=>["Mark McGwire", "Sammy Sosa"],
+          "rbi"=>["Sammy Sosa", "Ken Griffey"]}
+      assert actual == expected
+    end
+
+    test "2.11. Mapping between Sequences" do
+      actual = decode("2.11")
+      expected =
+        %{["Detroit Tigers", "Chicago cubs"] => ["2001-07-23"],
+          ["New York Yankees", "Atlanta Braves"] => ["2001-07-02", "2001-08-12", "2001-08-14"]}
+      assert actual == expected
+    end
+
+    test "2.12. Compact Nested Mapping" do
+      actual = decode("2.12")
       expected =
         [%{"item" => "Super Hoop", "quantity" => 1},
          %{"item" => "Basketball", "quantity" => 4},
@@ -17,20 +102,26 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "folded_new_lines_are_preserved_for_more_indented_and_blank_lines" do
-      actual = decode("folded_new_lines_are_preserved_for_more_indented_and_blank_lines")
-      expected = "Sammy Sosa completed another fine season with great stats.\n\n  63 Home Runs\n  0.288 Batting Average\n\nWhat a year!\n"
-      assert actual == expected
-    end
-
-    test "in_literals_newlines_are_preserved" do
-      actual = decode("in_literals_newlines_are_preserved")
+    test "2.13.  In literals, newlines are preserved" do
+      actual = decode("2.13")
       expected = "\\//||\\/||\n// ||  ||__\n"
       assert actual == expected
     end
 
-    test "indentation_determines_scope" do
-      actual = decode("indentation_determines_scope")
+    test "2.14.  In the folded scalars, newlines become spaces" do
+      actual = decode("2.14")
+      expected = "Mark McGwire's year was crippled by a knee injury.\n"
+      assert actual == expected
+    end
+
+    test "2.15.  Folded newlines are preserved for 'more indented' and blank lines" do
+      actual = decode("2.15")
+      expected = "Sammy Sosa completed another fine season with great stats.\n\n  63 Home Runs\n  0.288 Batting Average\n\nWhat a year!\n"
+      assert actual == expected
+    end
+
+    test "2.16.  Indentation determines scope" do
+      actual = decode("2.16")
       expected =
         %{"name"=>"Mark McGwire",
           "accomplishment"=>"Mark set a major league home run record in 1998.\n",
@@ -38,68 +129,8 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "in_the_folded_scalars_new_lines_become_spaces" do
-      actual = decode("in_the_folded_scalars_new_lines_become_spaces")
-      expected = "Mark McGwire's year was crippled by a knee injury.\n"
-      assert actual == expected
-    end
-
-    test "mapping_between_sequences" do
-      actual = decode("mapping_between_sequences")
-      expected =
-        %{["Detroit Tigers", "Chicago cubs"] => ["2001-07-23"],
-          ["New York Yankees", "Atlanta Braves"] => ["2001-07-02", "2001-08-12", "2001-08-14"]}
-      assert actual == expected
-    end
-
-    test "mapping_of_mappings" do
-      actual = decode("mapping_of_mappings")
-      expected =
-        %{"Mark McGwire"=> %{"hr"=>65, "avg"=>0.278},
-          "Sammy Sosa"=> %{"hr"=>63, "avg"=>0.288}}
-      assert actual == expected
-    end
-
-    test "mapping_scalars_to_scalars" do
-      actual = decode("mapping_scalars_to_scalars")
-      expected = %{"hr" => 65, "avg" => 0.278, "rbi" => 147}
-      assert actual == expected
-    end
-
-    test "mapping_scalars_to_sequencces" do
-      actual = decode("mapping_scalars_to_sequencces")
-      expected =
-        %{"american"=> ["Boston Red Sox", "Detroit Tigers", "New York Yankees"],
-          "national"=> ["New York Mets", "Chicago Cubs", "Atlanta Braves"]}
-      assert actual == expected
-    end
-
-    test "multi_line_flow_scalars" do
-      actual = decode("multi_line_flow_scalars")
-      expected =
-        %{"plain"=>"This unquoted scalar spans many lines.",
-          "quoted"=>"So does this quoted scalar.\n"}
-      assert actual == expected
-    end
-
-    test "node_for_sammy_sosa_appears_twice_in_this_document" do
-      actual = decode("node_for_sammy_sosa_appears_twice_in_this_document")
-      expected =
-        %{"hr"=>["Mark McGwire", "Sammy Sosa"],
-          "rbi"=>["Sammy Sosa", "Ken Griffey"]}
-      assert actual == expected
-    end
-
-    test "play_by_play_feed_from_a_game" do
-      actual = decode("play_by_play_feed_from_a_game")
-      expected =
-        [%{"time"=> "20:03:20", "player"=> "Sammy Sosa", "action"=> "strike (miss)"},
-         %{"time"=> "20:03:47", "player"=> "Sammy Sosa", "action"=> "grand slam"}]
-      assert actual == expected
-    end
-
-    test "quoted_scalars" do
-      actual = decode("quoted_scalars")
+    test "2.17. Quoted Scalars" do
+      actual = decode("2.17")
       expected =
         %{"unicode"=>"Sosa did fine.☺",
           "control"=>"\b1998\t1999\t2000\n",
@@ -110,42 +141,11 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "sequence_of_scalars" do
-      actual = decode("sequence_of_scalars")
-      expected = ["Mark McGwire", "Sammy Sosa", "Ken Griffey"]
-      assert actual == expected
-    end
-
-    test "sequence_of_mappings" do
-      actual = decode("sequence_of_mappings")
+    test "2.18. Multi-line Flow Scalars" do
+      actual = decode("2.18")
       expected =
-        [%{"name"=> "Mark McGwire", "hr"=> 65, "avg"=> 0.278},
-         %{"name"=> "Sammy Sosa", "hr"=> 63, "avg"=> 0.288}]
-      assert actual == expected
-    end
-
-    test "sequence_of_sequences" do
-      actual = decode("sequence_of_sequences")
-      expected =
-        [["name", "hr", "avg"],
-         ["Mark McGwire", 65, 0.278],
-         ["Sammy Sosa", 63, 0.288]]
-      assert actual == expected
-    end
-
-    test "single_document_with_two_comments" do
-      actual = decode("single_document_with_two_comments")
-      expected =
-        %{"hr"=>["Mark McGwire", "Sammy Sosa"],
-          "rbi"=>["Sammy Sosa", "Ken Griffey"]}
-      assert actual == expected
-    end
-
-    test "two_documents_in_a_stream" do
-      actual = decode("two_documents_in_a_stream")
-      expected =
-        [["Mark McGwire", "Sammy Sosa", "Ken Griffey"],
-         ["Chicago Cubs", "St Louis Cardinals"]]
+        %{"plain"=>"This unquoted scalar spans many lines.",
+          "quoted"=>"So does this quoted scalar.\n"}
       assert actual == expected
     end
   end
