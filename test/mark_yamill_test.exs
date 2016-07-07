bcdcecc6cde24b0a09aaf5a7726f812f7d748787
Michael Pope <mpope.cr@gmail.com>
2016-07-06 23:37:21 -0700

    Add end to end tests && clean up tests

--- test/mark_yamill_test.exs
+++ test/mark_yamill_test.exs
@@ -36,7 +36,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.5. Sequence of Sequences" do
+    test "2.5.  Sequence of Sequences" do
       actual = decode("2.5")
       expected =
         [["name", "hr", "avg"],
@@ -45,7 +45,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.6. Mapping of Mappings" do
+    test "2.6.  Mapping of Mappings" do
       actual = decode("2.6")
       expected =
         %{"Mark McGwire"=>%{"hr"=>65, "avg"=>0.278},
@@ -89,7 +89,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.11. Mapping between Sequences" do
+    test "2.11.  Mapping between Sequences" do
       actual = decode("2.11")
       expected =
         %{["Detroit Tigers",
@@ -99,7 +99,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.12. Compact Nested Mapping" do
+    test "2.12.  Compact Nested Mapping" do
       actual = decode("2.12")
       expected =
         [%{"item"=>"Super Hoop", "quantity"=>1},
@@ -135,7 +135,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.17. Quoted Scalars" do
+    test "2.17.  Quoted Scalars" do
       actual = decode("2.17")
       expected =
         %{"unicode"=>"Sosa did fine.☺",
@@ -147,7 +147,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.18. Multi-line Flow Scalars" do
+    test "2.18.  Multi-line Flow Scalars" do
       actual = decode("2.18")
       expected =
         %{"plain"=>"This unquoted scalar spans many lines.",
@@ -155,7 +155,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.19. Integers" do
+    test "2.19.  Integers" do
       actual = decode("2.19")
       expected =
         %{"canonical"=>12345,
@@ -165,7 +165,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.20. Floating Point" do
+    test "2.20.  Floating Point" do
       actual = decode("2.20")
       expected =
         %{"canonical"=>1230.15,
@@ -176,7 +176,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.21. Miscellaneous" do
+    test "2.21.  Miscellaneous" do
       actual = decode("2.21")
       expected =
         %{nil=>nil,
@@ -185,7 +185,7 @@ defmodule MarkYamillTest do
       assert actual == expected
     end
 
-    test "2.22. Miscellaneous" do
+    test "2.22.  Timestamps" do
       actual = decode("2.22")
       expected =
         %{"canonical"=>"2001-12-15T02:59:43.1Z",
@@ -196,32 +196,137 @@ defmodule MarkYamillTest do
     end
 
     @doc """
-    Because `!!binary` is an undefined tags, decoding `test/data/2.23.yml` will throw an error.
+    Because `binary` is an undefined tag, decoding `test/data/2.23-1.yml` will throw an error.
     """
-    test "2.23. Various Explicit Tags" do
-      assert catch_throw(decode("2.23"))
+    test "2.23-1.  Various Explicit Tags (w/ tags)" do
+      assert catch_throw(decode("2.23-1"))
     end
 
     @doc """
-    Because `!binary` is an undefined tags, decoding `test/data/2.23.yml` will throw an error.
+    See `test/data/2.23-1.yml` for test w/ tags
     """
-    test "2.24. Miscellaneous" do
-      assert catch_throw(decode("2.24"))
+    test "2.23-2.  Various Explicit Tags (w/ tags removed)" do
+      actual = decode("2.23-2")
+      expected =
+        %{"application specific tag"=>"The semantics of the tag\nabove may be different for\ndifferent documents.\n",
+          "not-date"=>"2002-04-28",
+          "picture"=>"R0lGODlhDAAMAIQAAP//9/X\n17unp5WZmZgAAAOfn515eXv\nPz7Y6OjuDg4J+fn5OTk6enp\n56enmleECcgggoBADs=\n"}
+      assert actual == expected
+    end
+
+    @doc """
+    Because `shape` is an undefined tag, decoding `test/data/2.24-1.yml` will throw an error.
+    """
+    test "2.24-1.  Global Tags (w/ tags)" do
+      assert catch_throw(decode("2.24-1"))
+    end
+
+    @doc """
+    See `test/data/2.24-1.yml` for test w/ tags
+    """
+    test "2.24-2.  Global Tags (w/ tags removed)" do
+      actual = decode("2.24-2")
+      expected =
+        [%{"center"=>%{"x"=>73, "y"=>129}, "radius"=>7},
+         %{"finish"=>%{"x"=>89, "y"=>102}, "start"=>"x"},
+         %{"color"=>16772795, "start"=>"x", "text"=>"Pretty vector drawing."}]
+      assert actual == expected
     end
 
 
     @doc """
-    Because `!!set` is an undefined tags, decoding `test/data/2.23.yml` will throw an error.
+    Because `set` is an undefined tag, decoding `test/data/2.25-1.yml` will throw an error.
     """
-    test "2.25. Unordered Sets" do
-      assert catch_throw(decode("2.25"))
+    test "2.25-1.  Unordered Sets (w/ tags)" do
+      assert catch_throw(decode("2.25-1"))
     end
 
     @doc """
-    Because `!!` is an undefined tags, decoding `test/data/2.23.yml` will throw an error.
+    See `test/data/2.25-1.yml` for test w/ tags
     """
-    test "2.26. Ordered Mappings" do
-      assert catch_throw(decode("2.26"))
+    test "2.25-2.  Unordered Sets (w/ tags removed)" do
+      actual = decode("2.25-2")
+      expected =
+        %{"Ken Griff"=>nil, "Mark McGwire"=>nil, "Sammy Sosa"=>nil}
+      assert actual == expected
+    end
+
+    @doc """
+    Because `omap` is an undefined tag, decoding `test/data/2.26-1.yml` will throw an error.
+    """
+    test "2.26-1.  Ordered Mappings (w/ tags removed)" do
+      assert catch_throw(decode("2.26-1"))
+    end
+
+    @doc """
+    See `test/data/2.26-1.yml` for test w/ tags
+    """
+    test "2.26-2.  Ordered Mappings (w/ tags removed)" do
+      actual = decode("2.26-2")
+      expected =
+        [%{"Mark McGwire"=>65}, %{"Sammy Sosa"=>63}, %{"Ken Griffy"=>58}]
+      assert actual == expected
+    end
+
+    @doc """
+    Because `invoice` is an undefined tag, decoding `test/data/2.27-1.yml` will throw an error.
+    """
+    test "2.27-1.  Invoice (w/ tags)" do
+      assert catch_throw(decode("2.27-1"))
+    end
+
+    @doc """
+    See `test/data/2.27-1.yml` for test w/ tags
+    """
+    test "2.27-2.  Invoice (w/ tags removed)" do
+      actual = decode("2.27-2")
+      expected =
+        %{"bill-to"=>%{"address"=>%{"city"=>"Royal Oak",
+                                    "lines"=>"458 Walkman Dr.\nSuite #292\n", "postal"=>48046,
+                                    "state"=>"MI"},
+                       "family"=>"Dumars",
+                       "given"=>"Chris"},
+        "comments"=>"Late afternoon is best. Backup contact is Nancy Billsmer @ 338-4338.",
+        "date"=>"2001-01-23",
+        "invoice"=>34843,
+        "product"=>[%{"description"=>"Basketball",
+                      "price"=>450.0,
+                      "quantity"=>4,
+                      "sku"=>"BL394D"},
+                    %{"description"=>"Super Hoop",
+                      "price"=>2392.0,
+                      "quantity"=>1,
+                      "sku"=>"BL4438H"}],
+        "ship-to"=>%{"address"=>%{"city"=>"Royal Oak",
+                                  "lines"=>"458 Walkman Dr.\nSuite #292\n",
+                                  "postal"=>48046,
+                                  "state"=>"MI"},
+                     "family"=>"Dumars",
+                     "given"=>"Chris"},
+        "tax"=>251.42,
+        "total"=>4443.52}
+      assert actual == expected
+    end
+
+    test "2.28.  Log File" do
+      actual = decode("2.28")
+      expected =
+        [%{"Time"=>"2001-11-23 15:01:42 -5",
+           "User"=>"ed",
+           "Warning"=>"This is an error message for the log file"},
+         %{"Time"=>"2001-11-23 15:02:31 -5",
+           "User"=>"ed",
+           "Warning"=>"A slightly different error message."},
+         %{"Date"=>"2001-11-23 15:03:17 -5",
+           "Fatal"=>"Unknown variable \"bar\"",
+           "Stack"=>[%{"code"=>"x = MoreObject(\"345\\n\")\n",
+                       "file"=>"TopClass.py",
+                       "line"=>23},
+                     %{"code"=>"foo = bar",
+                       "file"=>"MoreClass.py",
+                       "line"=>58}],
+           "User"=>"ed"}]
+      assert actual == expected
     end
   end
 end
